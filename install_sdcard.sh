4eddcb06c93d50c8dfba318ba74c38126974c234
陳鈞 <jim60105@gmail.com>
2023-01-01 11:44:14 +0800

    sudo umount /dev/mmcblk0p

--- install_sdcard.sh
+++ install_sdcard.sh
@@ -7,5 +7,9 @@ sudo rm /usr/lib/systemd/system/udisks2.service
 systemctl daemon-reload
 systemctl reset-failed
 
+sudo umount /dev/mmcblk0p6
+sudo umount /dev/mmcblk0p7
+sudo umount /dev/mmcblk0p8
+
 sudo sed -i 's/nvme0n1/mmcblk0/g' ~/tools/repair_device.sh
 sudo ~/tools/repair_device.sh all
