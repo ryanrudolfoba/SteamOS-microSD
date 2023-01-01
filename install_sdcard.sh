9543bb4ef526d52157b318c45fdd2e56f713f0b1
陳鈞 <jim60105@gmail.com>
2023-01-01 11:37:22 +0800

    systemctl stop udisks2.service
    fix

--- install_sdcard.sh
+++ install_sdcard.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+sudo systemctl stop udisks2.service
+sudo systemctl disable udisks2.service
+sudo rm /etc/systemd/system/udisks2.service
+sudo rm /usr/lib/systemd/system/udisks2.service
+systemctl daemon-reload
+systemctl reset-failed
+
 sudo sed -i 's/nvme0n1/mmcblk0/g' ~/tools/repair_device.sh
 sudo ~/tools/repair_device.sh all
